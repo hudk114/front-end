7ded4bebc5371f014155a58dba508974a138f00d
dekaihu <hudk@dekaihudeMacBook-Pro.local>
2018-01-12 21:24:46 +0800

    upd8 this

--- js/function/this.js
+++ js/function/this.js
@@ -1,40 +1,88 @@
-/** this of function
- *  @author hudk
- *  @link https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Operators/this
+/** 
+ * this of function
+ * @author hudk
+ * @link https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Operators/this
+ * @link http://blog.csdn.net/qaserfvvbbert/article/details/52385699
  */
 
-// this是基于函数运行时的执行环境绑定的
+// 函数简单调用下绑定到全局
 
 // strict mode下绑定
 function f() {
-  "use strict"; // 只有在调用的运行中使用strict mode才会绑定到undefined
+  'use strict'; // 只有在调用的运行中使用strict mode才会绑定到undefined
   console.log(this.a);
 }
 var a = 2;
-(function() {
+(function () {
   f();
 })(); // TypeError
 
-"use strict"
+'use strict'
 function f() {
   console.log(this.a);
 }
 var a = 2;
-(function() {
-  "use strict"
+(function () {
+  'use strict'
   f();
 })(); // 2
 
-// callback lose this
+// 使用apply, call, bind可以改变this值(显式绑定)
+function f() { }
+f.apply(obj);
+f.call(obj);
+var f1 = f.bind(obj); // f1 is a function which is always bind to obj
+f1();
+// 通过call与apply绑定后的函数无法重新绑定
+function f() {
+  console.log(this.a);
+}
+var obj = { a: 1 };
 function f1() {
-  console.log(this);
+  f.call(obj);
 }
-function f2(fn) {
-  // fn();
-  f1();
+var a = 2;
+f1.call(window); // 1
+var f2 = f1.bind(obj);
+f2 = f1.bind(window);
+f2(); // 1
+
+// 作为对象方法调用会绑定到对象
+
+var obj = {
+  func: function () { console.log(this); }
 }
-function f3() {
-  f2();
+obj.func(); // obj
+// 丢失
+(obj.func)(); // obj
+var c;
+(c = obj.func)(); // window
+(obj.func = obj.func)(); // window
+
+// callback lose this
+var obj = {
+  f() {
+    console.log(this);
+  }
 }
-f3();
-// f2(f1); // windows
+function f(fn) {
+  fn(); // same as (fn = obj.f)()
+}
+f(obj.f); // windows
+
+// 构造函数 绑定到返回到对象
+
+
+// array(lambda)
+// 无自己的this，绑定到定义时的外层this
+// 之后再用apply，call与bind方法也无效
+var obj = {
+  a: function () {
+    return (() => console.log(this));
+  }
+}
+obj.a()(); // obj
+var f = obj.a();
+f(); // obj 在obj的调用过程中外层this绑定了！
+var f2 = obj.a;
+f2()(); // window
